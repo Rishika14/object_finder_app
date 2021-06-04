9beb0150e260b75877ee6cc81ea171f3f1b502ad
Rishika14 <74129664+Rishika14@users.noreply.github.com>
2021-06-04 19:36:48 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,29 @@
+video="";
+
+function setup()
+{
+    canvas=createCanvas(480, 310);
+    canvas.center();
+    video = createCapture(VIDEO);
+    video.hide();
+}
+
+function draw()
+{
+    image(video, 0, 0, 480, 310);
+}
+
+function start()
+{
+    objectDetector = ml5.objectDetector('cocossd', modelLoaded);
+    document.getElementById("status").innerHTML = "Status: Detecting Objects";
+}
+
+function modelLoaded()
+{
+    console.log("Model Loaded!!");
+    status=true;
+    video.loop();
+    video.speed(1);
+    video.volume(0);
+}
